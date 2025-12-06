18bf84551d07a8afbc70cbb8c57f1241f44d9ccd
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-12-05 23:05:59 -0330

    Added
    -Import pictures and game pool

---
+++ game_data.js
@@ -0,0 +1,98 @@
+const catImages = [
+    "catIMGS/cat1.png",
+    "catIMGS/cat2.png",
+    "catIMGS/cat3.PNG",
+    "catIMGS/cat4.png",
+    "catIMGS/cat5.png",
+    "catIMGS/cat6.png",
+    "catIMGS/cat7.png",
+    "catIMGS/cat8.png",
+    "catIMGS/cat9.png",
+    "catIMGS/cat10.png",
+    "catIMGS/cat11.png",
+    "catIMGS/cat12.png",
+    "catIMGS/cat13.png",
+    "catIMGS/cat14.png",
+    "catIMGS/cat15.png",
+    "catIMGS/cat16.png"
+    ];
+
+const questionPool = [
+    {
+        question: "Which Alaskan town had a cat as its mayor for almost 20 years?",
+        correct: "Talkeetna, Alaska",
+        wrong: ["Juneau, Alaska", "Fairbanks, Alaska", "Anchorage, Alaska"]
+    },
+    {
+        question: "Roughly how many pet cats live in the United States?",
+        correct: "About 88 million",
+        wrong: ["23 million", "4 million", "85 million"]
+    },
+    {
+        question: "When was the first known cat video recorded?",
+        correct: "1894",
+        wrong: ["1915", "1942", "1888"]
+    },
+    {
+        question: "Usually, how many kittens are usually born in a single litter?",
+        correct: "3 to 4 kittens",
+        wrong: ["Exactly 1", "6 to 8 kittens", "10 or more kittens"]
+    },
+    {
+        question: "Which of these words can refer to a group of kittens?",
+        correct: "A kindle",
+        wrong: ["A pack", "A flock", "A school"]
+    },
+    {
+        question: "What color eyes are all kittens born with?",
+        correct: "Blue",
+        wrong: ["Green", "Yellow", "Brown"]
+    },
+    {
+        question: "What was the name of the first cat sent into space?",
+        correct: "Félicette",
+        wrong: ["Suki", "Sage", "Tobi"],
+    },
+    {
+        question: "What is the main purpose a cat's tail serves?",
+        correct: "Helping with balance and communication",
+        wrong: ["Storing extra food","Keeping their ears warm","Help clean themselves"],
+    },
+    {
+        question: "Which cat breed is traditionally given to newlyweds for good luck?",
+        correct: "Korat",
+        wrong: ["Siamese", "Burmese", "Bengal"],
+    },
+    {
+        question: "Which smell do many cats dislike so much that it keeps them away?",
+        correct: "Oranges",
+        wrong: ["Chicken Nuggets", "Lavender", "Cheese"],
+    },
+    {
+        question: "Which part of a cat's body is as unique as a human fingerprint?",
+        correct: "Its nose",
+        wrong: ["Its paws", "Its whiskers", "Its ears"],
+    },
+    {
+        question: "Which cats are more likely to be left-pawed?",
+        correct: "Male cats",
+        wrong: ["Female cats", "Both are just as likely", "Only new born kittens"],
+    },
+    {
+        question: "Which taste can cats NOT detect?",
+        correct: "Sweet",
+        wrong: ["Salty", "Sour", "Bitter"],
+    },
+    {
+        question: "Which cat breed is often called “the gentle giant”?",
+        correct: "Maine Coon",
+        wrong: ["Sphynx", "Tiger", "Jaguar"],
+    },
+    {
+        question: "What is the name for an extreme fear of cats?",
+        correct: "Ailurophobia",
+        wrong: ["Arachnophobia", "Acrophobia", "Felinophobia"],
+    }
+];
+
+export {catImages, questionPool};
